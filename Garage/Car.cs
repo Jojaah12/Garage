9951b078c90ba6a79aa1ade94c52b2112652ea28
Jojaah12 <david.aljojah@gmail.com>
2024-03-21 23:56:42 +0100

    Created subclass of Car

---
+++ Garage/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class Car : Vehicle
+    {
+        public int NumberOfSeats { get; set; }
+
+        public Car(string registrationNumber, string color, int numberOfWheels, int numberOfSeats)
+            : base(registrationNumber, color, numberOfWheels)
+        {
+            NumberOfSeats = numberOfSeats;
+        }
+    }
+}
