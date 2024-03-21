1a337c1ef48496b9948777b2cf3b721760ede82c
Jojaah12 <david.aljojah@gmail.com>
2024-03-21 23:57:47 +0100

    Created Subclass of bus

---
+++ Garage/Bus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class Bus : Vehicle
+    {
+        public int Capacity { get; set; }
+
+        public Bus(string registrationNumber, string color, int numberOfWheels, int capacity)
+            : base(registrationNumber, color, numberOfWheels)
+        {
+            Capacity = capacity;
+        }
+    }
+}
