726a3f179938a9b059ce277ac4dca88e771a7c5c
Jojaah12 <david.aljojah@gmail.com>
2024-03-26 20:54:13 +0100

    Added subclass mototcycle

---
+++ Garage/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class Motorcycle : Vehicle
+    {
+        public bool HasSideCar { get; set; }
+
+        public Motorcycle(string registrationNumber, string color, int numberOfWheels, bool hasSideCar)
+            : base(registrationNumber, color, numberOfWheels)
+        {
+            HasSideCar = hasSideCar;
+        }
+    }
+}
