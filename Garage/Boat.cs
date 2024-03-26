dbad38bb44731adb30621fed1feeda71106e1d7c
Jojaah12 <david.aljojah@gmail.com>
2024-03-26 09:55:31 +0100

    implemented Boat subclass

---
+++ Garage/Boat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class Boat : Vehicle
+    {
+        public double Length { get; set; }
+
+        public Boat(string registrationNumber, string color, int numberOfWheels, double length)
+            : base(registrationNumber, color, numberOfWheels)
+        {
+            Length = length;
+        }
+    }
+}
